efaee6fd7594a0913ea22b0e3c71b5596b8af3f9
sethdame <seth.e.dame@gmail.com>
2015-10-20 23:10:19 -0600

    design tweaks

--- resume/js/resumeCtrl.js
+++ resume/js/resumeCtrl.js
@@ -1,13 +1,13 @@
 var resumeApp = angular.module('resumeApp');
 
 resumeApp.controller('resumeCtrl', function($scope) {
-    $scope.message = 'What is most important at the moment';
+    $scope.message = 'Pertinent Experience';
 });
 
 resumeApp.controller('experienceCtrl', function($scope) {
-    $scope.message = 'Wanting this to be filled tech XP!';
+    $scope.message = 'Wanting this to be filled with Tech XP!';
 });
 
 resumeApp.controller('educationCtrl', function($scope) {
-    $scope.message = 'binding ----> waiting.';
+    $scope.message = 'binding ----> working.';
 });
