152385482de81bb832ca7f0cb5bc489547533c83
sethdame <seth.e.dame@gmail.com>
2015-10-28 01:57:10 -0600

    project accordion

---
+++ js/projApp.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('#projSlider').zAccordion({
+		width: 900,
+		speed: 1500,
+		timeout: 20000,
+		slideClass: "slider",
+		animationStart: function() {
+			$('#projSlider').find('li.slide-previous div').fadeOut();
+		},
+		animationComplete: function() {
+			$('#projSlider').find('li.slide-open div').fadeIn();
+		},
+		buildComplete: function() {
+			$('#projSlider').find('li.slide-closed div').css('display', 'none');
+			$('#projSlider').find('li.slide-open div').fadeIn();
+		},
+		slideWidth: 600,
+		height: 400,
+		easing: 'easeOutBounce'
+	});
+});
